fe9f156d96ee5262ad159e7b479e0125c7cb4a95
shahryary <shahryary@gmail.com>
2019-10-15 10:54:45 +0200

    Added equivalence '=' operator

--- R/operators.R
+++ R/operators.R
@@ -19,3 +19,13 @@
     as.logical()
   return(pred)
 }
+
+#' @describeIn deepoperators
+#' @export
+`%=%` <- function(x, y) {
+  X <- matrix(as.integer(c(x, y)), ncol = 2)
+  pred <- deepoperators.env$models$eq %>%
+    predict_classes(X) %>%
+    as.logical()
+  return(pred)
+}
